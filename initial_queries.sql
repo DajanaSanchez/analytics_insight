b993cb8714e0d52a7bf0a1fb4f4a431b129bac07
DajanaSanchez <djsanchez0808@outlook.com>
2022-03-10 21:18:37 -0500

    initial commit to repository

--- initial_queries.sql
+++ initial_queries.sql
@@ -1,10 +1,9 @@
-SELECT website_sessions.utm_content,
-COUNT(DISTINCT website_sessions.website_session_id) AS sessions,
-COUNT(DISTINCT orders.order_id) AS orders,
-COUNT(DISTINCT orders.order_id)/COUNT(DISTINCT website_sessions.website_session_id) AS session_to_order_conv_rt
+## calculate conversion rate from session to order for gsearch nonbrand
+SELECT COUNT(DISTINCT(website_sessions.website_session_id)) AS sessions, COUNT(DISTINCT(orders.website_session_id)) AS orders,
+COUNT(orders.website_session_id) / COUNT(website_sessions.website_session_id) AS conversion_rate
 FROM website_sessions
 LEFT JOIN orders
 ON orders.website_session_id = website_sessions.website_session_id
-WHERE website_sessions.website_session_id BETWEEN 1000 AND 2000
-GROUP BY 1
-ORDER BY 2 DESC;
+WHERE website_sessions.created_at < '2012-04-14'
+AND utm_source = 'gsearch'
+AND utm_campaign = 'nonbrand';
