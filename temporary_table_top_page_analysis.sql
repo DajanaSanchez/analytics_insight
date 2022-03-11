b1b3bd5c08fe77e31c41860a99923f4712ce87ff
DajanaSanchez <djsanchez0808@outlook.com>
2022-03-11 15:57:22 -0500

    initial commit to repository

--- temporary_table_top_page_analysis.sql
+++ temporary_table_top_page_analysis.sql
@@ -1,11 +1,14 @@
-##  pull the most-viewed website pages ranked by session volume. through 2012-06-09
+## pull all pages where customer lands and rank by entry volume, created less than 2012-06-12
+## find the first pageviw for each session and then find the url that customer saw on that first pageview
 
-CREATE TEMPORARY TABLE most_viewed_pgs
-SELECT website_pageview_id, pageview_url,website_session_id
-       FROM website_pageviews
-WHERE created_at < '2012-06-09';
+CREATE TEMPORARY TABLE first_view_by_session
+SELECT website_session_id, MIN(website_pageview_id) AS first_view
+FROM website_pageviews
+WHERE created_at < '2012-06-12'
+GROUP BY website_session_id;
 
-SELECT pageview_url, COUNT(DISTINCT(website_session_id)) AS session_volume
-FROM most_viewed_pgs
-GROUP BY pageview_url
-ORDER BY session_volume DESC;
+SELECT website_pageviews.pageview_url, COUNT(DISTINCT(first_view_by_session.website_session_id)) AS sessions
+FROM first_view_by_session
+LEFT JOIN website_pageviews
+ON website_pageviews.website_pageview_id = first_view_by_session.first_view
+GROUP BY website_pageviews.pageview_url;
