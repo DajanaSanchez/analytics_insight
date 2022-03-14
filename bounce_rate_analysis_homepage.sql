fc3aee2cde5d0f48d9ab807f246c09762b59e58a
DajanaSanchez <djsanchez0808@outlook.com>
2022-03-14 14:41:03 -0400

    Revert "changed redundancy in script"
    This reverts commit 6af1d2c9b79aaa082928de3700bbe0fa9ca00af1.

--- bounce_rate_analysis_homepage.sql
+++ bounce_rate_analysis_homepage.sql
@@ -53,4 +53,6 @@ SELECT COUNT(DISTINCT(landing_page_seshhhh.website_session_id)) AS sessions, COU
        COUNT(DISTINCT(bounced_sessions.website_session_id)) / COUNT(DISTINCT(landing_page_seshhhh.website_session_id)) AS bounced_rate
 FROM landing_page_seshhhh
 LEFT JOIN bounced_sessions
-ON landing_page_seshhhh.website_session_id = bounced_sessions.website_session_id;
+ON landing_page_seshhhh.website_session_id = bounced_sessions.website_session_id
+ORDER BY landing_page_seshhhh.website_session_id;
+
