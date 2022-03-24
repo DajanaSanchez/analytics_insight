f4998d0ace4853a451f2012236df784ae3d106dd
dajana <djsanchez0808@outlook.com>
2022-03-24 12:54:40 -0400

    analyzing the impact of releasing a new product to the overall sales

---
+++ product_analysis/product_level_website_pathing.sql
@@ -0,0 +1,6 @@
+#business objective - look at sessions which hit the /products page and see where they went next
+#pull click through rates from /products to since the new product launch on january 6th 2013 by product
+#compare to the 3 months leading up to launch as a baseline; data through april 6, 2013
+#
+# SELECT
+# FROM website_pageviews
